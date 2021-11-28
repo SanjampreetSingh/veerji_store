56118e56d0b2f0292c003aee92c751d9fcf846ba
Sanjampreet Singh <you@example.com>
2021-11-28 19:08:46 +0530

    renamed apps

--- api/recurring_product/apps.py
+++ api/recurring_product/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RecurringProductsConfig(AppConfig):
+class RecurringProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api.recurring_product'
