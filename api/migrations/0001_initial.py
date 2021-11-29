b1571e7f6967120c35ebd47e994000cf82f42471
Sanjampreet Singh <you@example.com>
2021-11-29 20:47:11 +0530

    Updated seed

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from django.db.models.functions.datetime import TruncTime
 import environ
 
 from api.user.models import User
+from api.locality.models import Locality
 
 # Initialise environment variables
 env = environ.Env()
@@ -12,12 +13,16 @@ environ.Env.read_env()
 class Migration(migrations.Migration):
 
     def seed_data(apps, schema_editor):
+        loc = Locality(name="G Block SBS Nagar")
+        loc.save()
+
         user = User(
             name=env('SU_NAME'),
             email=env('SU_EMAIL'),
             is_staff=True,
             is_superuser=True,
             phone=env('SU_PHONE'),
+            locality=loc
         )
         user.set_password(env('SU_PASSWORD'))
         user.save()
