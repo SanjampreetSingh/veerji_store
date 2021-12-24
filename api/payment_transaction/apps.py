220a543c68431cb3e28dec34767b667a851bc279
Sanjampreet Singh <you@example.com>
2021-12-25 02:58:45 +0530

    removed unrequired app

--- api/payment_transaction/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaymentTransactionConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api.payment_transaction'
--- api/payment_transaction/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from utils.model_utils.models import (
-    TimeStampedModel
-)
-
-
-class PaymentTransaction(TimeStampedModel, models.Model):
-    pass
