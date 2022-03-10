c5d8d5c6d8077835b7124f19c0d35ff8fcf67483
Sanjampreet Singh <sanjam531@gmail.com>
2022-03-10 18:43:24 +0530

    Product List

--- api/product/serializers.py
+++ api/product/serializers.py
@@ -7,11 +7,13 @@ class ProductSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(
         max_length=None, allow_empty_file=False, allow_null=True, required=False
     )
+    category_name = serializers.CharField(
+        source="category.name", read_only=True)
 
     class Meta:
         model = Product
         fields = (
-            'id', 'name', 'description', 'price', 'image', 'category'
+            'id', 'name', 'description', 'price', 'image', 'category', 'category_name'
         )
 
 
